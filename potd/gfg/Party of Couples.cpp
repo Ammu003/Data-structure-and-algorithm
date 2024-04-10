1f5a0db99e0e7d1781d89e05b6663f31116288d9
ammu <97016400+Ammu003@users.noreply.github.com>
2024-04-10 09:58:14 +0530

    Add files via upload

---
+++ potd/gfg/Party of Couples.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution{
+    public:
+    int findSingle(int n, int arr[]){
+        int ans=0;
+        for(int i=0;i<n;i++)
+        {
+            ans=ans^arr[i];
+        }
+        return ans;
+    }
+};
