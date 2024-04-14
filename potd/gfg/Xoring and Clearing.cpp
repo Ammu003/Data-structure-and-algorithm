ae94791cef0dce60c89b976f50bc98cd1244c995
ammu <97016400+Ammu003@users.noreply.github.com>
2024-04-14 23:49:55 +0530

    Add files via upload

---
+++ potd/gfg/Xoring and Clearing.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+  public:
+    void printArr(int n, int arr[]) {
+        for(int i=0;i<n;i++)
+        {
+            cout<<arr[i]<<" ";
+        }
+        cout<<endl;
+    }
+
+    void setToZero(int n, int arr[]) {
+        for(int i=0;i<n;i++)
+        {
+            arr[i]=0;
+        }
+    }
+
+    void xor1ToN(int n, int arr[]) {
+        for(int i=0;i<n;i++)
+        {
+            arr[i]=arr[i]^i;
+        }
+    }
+};
